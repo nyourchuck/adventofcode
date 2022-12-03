e0741e20d447c66eb52700f2dd5e02ec0c1576f7
Nate Yourchuck <nate@nimblegorilla.com>
2022-12-03 13:38:42 -0600

    Advent of Code 2022 Day 3

---
+++ 2022/day3.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "pry"
+require_relative "../lib/puzzle"
+
+class Day3 < Puzzle
+  def filter
+    lines.map(&:chars)
+  end
+
+  def priority(item)
+    code = item.ord
+    if code >= 97
+      code - 97 + 1
+    else
+      code - 65 + 27
+    end
+  end
+
+  def answer1
+    commons = data.map do |data|
+      a = data.slice!(0, data.length / 2)
+      priority a.intersection(data).first
+    end
+    commons.sum
+  end
+
+  def answer2
+    priorities = 0
+    while data.any?
+      (a, b, c) = data.slice!(0, 3)
+      badge = a.intersection(b).intersection(c).first
+      priorities += priority(badge)
+    end
+    priorities
+  end
+
+  if __FILE__ == $PROGRAM_NAME
+    input = new(:input)
+    sample = new(:sample)
+
+    puts "PART ONE"
+    sample.solve1
+    input.solve1
+
+    puts "PART TWO"
+    sample.solve2
+    input.solve2
+  end
+end
---
+++ spec/2022/day3_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "../../2022/day3"
+
+describe Day3 do
+  let(:sample) { Day3.new(:sample) }
+  let(:input) { Day3.new(:input) }
+
+  it "calculates priority" do
+    expect(sample.priority("a")).to eq 1
+    expect(sample.priority("z")).to eq 26
+    expect(sample.priority("A")).to eq 27
+    expect(sample.priority("Z")).to eq 52
+  end
+
+  it "calculates sample answer 1" do
+    expect(sample.answer1).to eq 157
+  end
+
+  it "calculates sample answer 2" do
+    expect(sample.answer2).to eq 70
+  end
+
+  it "calculates input answer 1" do
+    expect(input.answer1).to eq 7850
+  end
+
+  it "calculates input answer 2" do
+    expect(input.answer2).to eq 2581
+  end
+end
