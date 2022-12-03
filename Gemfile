a912491c6afb832cf28539e8fd6aaea2c52c258f
Nate Yourchuck <nate@nimblegorilla.com>
2022-12-03 10:19:21 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.7.2'
+
+gem 'activesupport'
+gem 'http'
+gem 'json'
+gem 'pastel'
+gem 'pry-byebug'
+gem 'rspec'
+gem 'ruby-prof'
+gem 'terminal-table'
---
+++ lib/puzzle.rb
@@ -0,0 +1,54 @@
+class Puzzle
+  attr_accessor :data
+  attr_accessor :input_type
+  attr_accessor :lines
+
+  def initialize(input_type)
+    self.input_type = input_type
+  end
+
+  # input filenames are stored in data directory
+  # example: `data/day1.sample` and `data/day1.input`
+  def input_filename
+    "data/#{self.class.to_s.downcase}.#{input_type}"
+  end
+
+  def lines
+    @lines ||= File.open(input_filename).readlines.map(&:chomp)
+  end
+
+  def integers(string)
+    string.split(/\D+/).map(&:to_i)
+  end
+
+  def data
+    @data ||= filter
+  end
+
+  def solve1
+    if defined? answer1
+      @data = nil
+      put_answer "  ANSWER1 (#{input_type}):", answer1
+    else
+      puts " ANSWER1 UNDEFINED"
+    end
+  end
+
+  def solve2
+    if defined? answer2
+      @data = nil
+      put_answer "  ANSWER2 (#{input_type}):", answer2
+    else
+      puts " ANSWER2 UNDEFINED"
+    end
+  end
+
+  def put_answer(prefix, answer)
+    if answer.respond_to?(:include?) && answer.include?("\n")
+      puts prefix
+      puts answer
+    else
+      puts "#{prefix} #{answer}"
+    end
+  end
+end
