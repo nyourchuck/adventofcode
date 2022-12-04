0918d5b9673d6b8af19cccc1f651abb7ce5af010
Nate Yourchuck <nate@nimblegorilla.com>
2022-12-04 00:02:22 -0600

    Advent of Code 2022 Day 4

---
+++ spec/2022/day4_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative "../../2022/day4"
+
+describe Day4 do
+  let(:sample) { Day4.new(:sample) }
+  let(:input) { Day4.new(:input) }
+
+  it "calculates sample answer 1" do
+    expect(sample.answer1).to eq 2
+  end
+
+  it "calculates sample answer 2" do
+    expect(sample.answer2).to eq 4
+  end
+
+  it "calculates input answer 1" do
+    expect(input.answer1).to eq 490
+  end
+
+  it "calculates input answer 2" do
+    expect(input.answer2).to eq 921
+  end
+end
