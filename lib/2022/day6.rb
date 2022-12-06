eb1375c06620e22910ca4c44f6efa1446557874b
Nate Yourchuck <nate@nimblegorilla.com>
2022-12-05 23:32:13 -0600

    Advent of Code 2022 Day 6

---
+++ lib/2022/day6.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Day6 < Puzzle
+  def filter
+    lines.first.split('')
+  end
+
+  def answer1(size: 4)
+    index = 0
+    while index < data.length
+      if data.slice(index, size).uniq.length == size
+        return index + size
+      end
+      index = index + 1
+    end
+  end
+
+  def answer2
+    answer1(size: 14)
+  end
+end
